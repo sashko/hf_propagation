3e98b9a970e7a990a5f0248323dc97556d76cf5e
Oleksandr Kravchuk <open.source@oleksandr-kravchuk.com>
2025-04-18 17:48:50 +0200

    Change app title

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "hf_propagation");
+    gtk_header_bar_set_title(header_bar, "HF Propagation");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "hf_propagation");
+    gtk_window_set_title(window, "HF Propagation");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
