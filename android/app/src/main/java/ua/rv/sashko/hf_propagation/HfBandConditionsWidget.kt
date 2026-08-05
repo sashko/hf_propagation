bdcee3060f6419c7b5c0b3f4f193623282cab782
Oleksandr Kravchuk <open.source@oleksandr-kravchuk.com>
2026-08-05 20:38:11 +0200

    Add HF band conditions widget

---
+++ android/app/src/main/java/ua/rv/sashko/hf_propagation/HfBandConditionsWidget.kt
@@ -0,0 +1,34 @@
+package ua.rv.sashko.hf_propagation
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+class HfBandConditionsWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+    }
+
+    override fun onDisabled(context: Context) {
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val views = RemoteViews(context.packageName, R.layout.hf_band_conditions_widget)
+    views.setTextViewText(R.id.appwidget_text, context.getString(R.string.hf_widget_name))
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
